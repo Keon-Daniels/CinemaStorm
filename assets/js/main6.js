7e9bc7a5859acf0c778f6b9ff30a8830f4872ec6
Keon Daniels <keongi46@gmail.com>
2018-08-07 17:52:05 +0000

    next commit

---
+++ assets/js/main6.js
@@ -0,0 +1,73 @@
+// let upVote = document.querySelector('#upVote')
+// let downVote = document.querySelector('#downVote')
+
+function getPostings() {
+    console.log("getPostings")
+    firebase.database().ref('/').on('value', (snapshot) => {
+        let data = snapshot.val()
+
+        let displayPostings = document.querySelector('#postings')
+        displayPostings.innerHTML = ""
+        let url = window.location.href
+        let id = getParameterByName('id', url)
+        for (key in data) {
+            console.log(key)
+            if (key == id) {
+                displayPostings.innerHTML += `<div class="card"  style="">
+              <div class="card-body" id = "${key}">
+                <div class="card-header">${data[key].title}</div>
+                <p class="card-text">${data[key].description}</p>
+                <p class="card-text"> <a href="mailto:${data[key].email}"> Email: ${data[key].email}</a> </p>
+                <p class="card-text"> Time Posted: ${data[key].time} </a> </p>
+                <div class="fb-share-button" data-href="http://cinemastorm.site/post-Page.html?id=${key}" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div>
+                <button class = "vote upVote" >👍 ${Object.keys(data[key].upVote).length}</button>
+                <button class = "vote downVote">👎 ${Object.keys(data[key].downVote).length}</button>
+              </div>
+            </div>
+            <br>`
+            }
+        }
+
+        // getupVote()
+        // getdownVote()
+    })
+
+}
+
+// function getupVote() {
+//     let upvotes = document.querySelectorAll('.upVote')
+//     upvotes.forEach(upvote => {
+//         upvote.addEventListener('click', (e) => {
+//             let key = e.target.parentNode.id
+//             let email = localStorage.getItem("email")
+//             firebase.database().ref("/" + key).child("upVote").push(email)
+//         })
+//     })
+// }
+
+// function getdownVote() {
+//     let downvotes = document.querySelectorAll('.downVote')
+//     downvotes.forEach(downvote => {
+//         downvote.addEventListener('click', (e) => {
+//             let key = e.target.parentNode.id
+//             let email = localStorage.getItem("email")
+//             firebase.database().ref("/" + key).child("downVote").push(email)
+//         })
+//     })
+// }
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+window.onload = function() {
+    getPostings()
+    let url = window.location.href
+    console.log(url)
+    console.log(getParameterByName('id', url))
+}
